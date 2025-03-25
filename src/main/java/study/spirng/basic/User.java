4b2b694b8bd322701650e338316f0820add27552
gyj <gyj@gmail.com>
2025-03-25 10:49:23 +0900

    타입리프 기본문법

---
+++ src/main/java/study/spirng/basic/User.java
@@ -0,0 +1,14 @@
+package study.spirng.basic;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor@NoArgsConstructor
+public class User {
+    private String username;
+    private int age;
+}
---
+++ src/test/java/study/spirng/SpirngApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spirng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
